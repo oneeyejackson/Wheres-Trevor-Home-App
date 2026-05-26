ca34ceddeacc95aee37c22d1f30cfe1f4549392b
oneeyejackson <oneeyejackson@gmail.com>
2026-05-26 15:34:45 -0700

    Load home final polish from patch layer

--- public/app-patch.js
+++ public/app-patch.js
@@ -28,4 +28,5 @@
   setInterval(()=>{if(localStorage.getItem('wt_guest_lock')==='1')showGuestLock()},900000);
   document.addEventListener('visibilitychange',()=>{if(!document.hidden)softRefresh('visible')});
   window.addEventListener('focus',()=>softRefresh('visible'));
+  const script=document.createElement('script');script.src='/home-final.js';script.defer=true;document.head.appendChild(script);
 })();
