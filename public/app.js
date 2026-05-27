5f4e373da8296de33b2f82438c404a26208858e8
oneeyejackson <oneeyejackson@gmail.com>
2026-05-26 19:59:41 -0700

    Directly load home final fix

--- public/app.js
+++ public/app.js
@@ -1,1 +1,1 @@
-import('/app-v3.js?v=20260526a').then(()=>import('/app-patch.js?v=20260526a'));
+import('/app-v3.js?v=20260526b').then(()=>import('/app-patch.js?v=20260526b')).then(()=>import('/home-final.js?v=20260526b'));
